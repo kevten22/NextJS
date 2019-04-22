808805eedf40b9bbc72ab336bf715ab186c10472
Kevin Tena <kevten09@gmail.com>
2019-04-22 12:54:48 -0500

    Added getInitialProps functions to index.js and post.js to fetch data

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6ba5b375217ef7dbc345";
+/******/ 	var hotCurrentHash = "5d742738ccff6c6a8bf6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
